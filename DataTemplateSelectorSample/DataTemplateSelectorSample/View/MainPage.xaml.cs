01a02a2292020860ead0508908874f11496aa603
Jeya Kasipandi <jeya.kasipandi@syncfusion.com>
2021-04-26 16:34:44 +0530

    DataTemplate selector has been added

---
+++ DataTemplateSelectorSample/DataTemplateSelectorSample/View/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Syncfusion.SfPicker.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataTemplateSelectorSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+           picker.IsOpen = true;
+        }
+       
+    }
+}
